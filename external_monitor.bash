41a71625cff3060b69c12cc5b8a476353a86cf98
paulmolloy <molloyp1@tcd.ie>
2020-05-20 14:34:12 +0100

    Add automatic setup for either intel or Nvidia.

--- external_monitor.bash
+++ external_monitor.bash
@@ -1,7 +1,17 @@
-xrandr --output DP-1-1 --scale 2x2 --mode 1920x1080 --fb 3840x2160 --pos 0x0
+#xrandr --output DP-1-1 --scale 2x2 --mode 1920x1080 --fb 3840x2160 --pos 0x0
 
-#xrandr --output DP-1 --scale 2x2 --mode 1920x1080 --fb 3840x2160 --pos 0x0
-xrandr --output DP-1-1 --right-of eDP-1-1
-# Get rid of flashing mouse.
-xrandr --output eDP-1-1 --scale 0.9999x0.9999
+GPU=$(prime-select query)
+if [ "$GPU" == "nvidia" ]
+then 
+    echo "Resize screen for Nvidia GPU"
+    /bin/screen-automation-pablo/nvidia_external_monitor.bash
+
+else
+    echo "Resize screen for Intel GPU"
+    xrandr --output DP-1 --scale 2x2 --mode 1920x1080 --fb 3840x2160 --pos 0x0
+    xrandr --output DP-1 --right-of eDP-1
+    # Get rid of flashing mouse.
+    xrandr --output eDP-1 --scale 0.9999x0.9999
+
+fi
 
---
+++ nvidia_external_monitor.bash
@@ -0,0 +1,7 @@
+xrandr --output DP-1-1 --scale 2x2 --mode 1920x1080 --fb 3840x2160 --pos 0x0
+
+#xrandr --output DP-1 --scale 2x2 --mode 1920x1080 --fb 3840x2160 --pos 0x0
+xrandr --output DP-1-1 --right-of eDP-1-1
+# Get rid of flashing mouse.
+xrandr --output eDP-1-1 --scale 0.9999x0.9999
+
